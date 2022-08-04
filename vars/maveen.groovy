a2a814ddaad09832dc3a59a2031f7affce61c884
joshking1 <88409463+joshking1@users.noreply.github.com>
2022-08-04 18:47:02 -0400

    Update maveen.groovy

--- vars/maveen.groovy
+++ vars/maveen.groovy
@@ -3,7 +3,7 @@
 import org.apache.commons.lang.StringUtils
 
 def call(String mvnaction) {
-    def mavenHome = tool name: "Maven-3.8.4", type: "maven"
+    def mavenHome = tool name: "Maven-3.8.6", type: "maven"
     def mavenCMD = "${mavenHome}/bin/mvn"
     sh "${mavenCMD} clean package"
     
